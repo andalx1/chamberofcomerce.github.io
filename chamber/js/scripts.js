5e6507e99db8393f5a9150a7929ed6c883825331
andalx1 <andrezbenavidex@gmail.com>
2021-07-07 01:32:28 -0500

    chamber

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -4,3 +4,12 @@ const navmenu = document.querySelector(".nav-menu")
 toogle.addEventListener("click", () => {
     navmenu.classList.toggle("nav-menu_visible")
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
